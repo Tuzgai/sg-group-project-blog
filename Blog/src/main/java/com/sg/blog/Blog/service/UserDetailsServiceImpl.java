f9f8cc6c7d8945106232b1a54ed00ce4007bc62c
Stuart Wilson <tuzgai@gmail.com>
2019-03-22 13:29:01 -0500

    Correct DB name

--- Blog/src/main/java/com/sg/blog/Blog/service/UserDetailsServiceImpl.java
+++ Blog/src/main/java/com/sg/blog/Blog/service/UserDetailsServiceImpl.java
@@ -1,5 +1,18 @@
 package com.sg.blog.Blog.service;
 
+import com.sg.blog.Blog.dao.UserDao;
+import com.sg.blog.Blog.entity.Role;
+import com.sg.blog.Blog.entity.User;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author Stuart
