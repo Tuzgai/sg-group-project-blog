4065c1d967bebbeb23a7664d18658aaaf1abb9e3
Stuart Wilson <tuzgai@gmail.com>
2019-03-31 15:05:20 -0500

    End of session commit

--- Blog/src/main/java/com/sg/blog/Blog/controller/AdminController.java
+++ Blog/src/main/java/com/sg/blog/Blog/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.sg.blog.Blog.controller;
 
+import com.sg.blog.Blog.dao.PostRepository;
 import com.sg.blog.Blog.dao.UserRepository;
+import com.sg.blog.Blog.entity.Post;
 import com.sg.blog.Blog.entity.User;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +17,16 @@ public class AdminController {
     @Autowired
     UserRepository userRepository;
     
+    @Autowired
+    PostRepository postRepository;
+    
     @GetMapping("/admin")
     String showAdminPage(Model model) {
         List<User> users = userRepository.findAll();
+        List<Post> posts = postRepository.findByApprovedFalse();
         
         model.addAttribute("users", users);
+        model.addAttribute("posts", posts);
         
         return "admin";
     }
--- Blog/src/main/java/com/sg/blog/Blog/dao/PostRepository.java
+++ Blog/src/main/java/com/sg/blog/Blog/dao/PostRepository.java
@@ -1,6 +1,7 @@
 package com.sg.blog.Blog.dao;
 
 import com.sg.blog.Blog.entity.Post;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer>{
-
+    public List<Post> findByApprovedFalse();
 }
