342838359cbd8fe429825d34b4a43845c015c3ba
Stuart Wilson <tuzgai@gmail.com>
2019-04-03 20:50:18 -0500

    CRUD for users

--- Blog/src/main/resources/model/BlogInsert.sql
+++ Blog/src/main/resources/model/BlogInsert.sql
@@ -3,8 +3,9 @@ Use Blog;
 insert into user(
 id, `name`, username, `password`, email, enabled)
 values
-(1, "Stu", "admin", "password", "stu@stu.systems", true),
-(2, "Kahn", "user", "password", "kh@gmail.com",  true);
+(1, "User deleted", "", "", "", true),
+(2, "Stu", "admin", "password", "stu@stu.systems", true),
+(3, "Kahn", "user", "password", "kh@gmail.com",  true);
 
 
 insert into role(id, role)
@@ -12,7 +13,8 @@ insert into role(id, role)
     (2,"ROLE_USER");
     
 insert into user_role(user_id, role_id)
-    values(1,1),(1,2),(2,2);
+    values(2,1),(2,2),(3,2);
     
-UPDATE `user` SET `password` = '$2a$10$C1triXdUhROmIwLT/mbndeJBGDBE8iKDSoHatzVU7kfUMibVjtX8q' WHERE id = 1;
+UPDATE `user` SET `password` = '4F2BBCC3B637B9E5F4A5326574F0FE84629F8710553F9EC57242180CF50FF2D8' WHERE id = 1;
 UPDATE `user` SET `password` = '$2a$10$C1triXdUhROmIwLT/mbndeJBGDBE8iKDSoHatzVU7kfUMibVjtX8q' WHERE id = 2;
+UPDATE `user` SET `password` = '$2a$10$C1triXdUhROmIwLT/mbndeJBGDBE8iKDSoHatzVU7kfUMibVjtX8q' WHERE id = 3;
